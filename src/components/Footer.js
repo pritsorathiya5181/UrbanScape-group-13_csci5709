a54cdfd833b2bf68196b4ea2db73005eb5fad81b
Prit Ajaykumar Sorathiya <pr669751@dal.ca>
2022-04-07 14:24:52 -0300

    Change localhost calls

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,10 @@
 
 import { MailOutline, Phone, Room } from '@mui/icons-material'
 import styled from 'styled-components'
-import {
-  Grid,
-  Link
-} from '@mui/material'
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-
+import { Grid, Link } from '@mui/material'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
 
 const Desc = styled.p`
   margin: 20px 0px;
@@ -35,70 +31,63 @@ const Payment = styled.img`
  */
 const Footer = () => {
   return (
-    <Grid sx={{ padding: "50px" }} container spacing={3}>
+    <Grid sx={{ padding: '50px' }} container spacing={3}>
       <Grid item xs={6}>
         <Title>About Us</Title>
         <Desc>
-          We understand that your time is precious and finding time for everyday chores in your busy schedule is challenging.
-          That's why we are here to ease your workload.
-          We provide beauty services like salon at home and home care services like plumbing and carpentry, all at your doorstep.
-          Our professionals are well articulated in their profession and they undergo rigorous training.
-          Your satisfaction means the world to us.
+          We understand that your time is precious and finding time for everyday
+          chores in your busy schedule is challenging. That's why we are here to
+          ease your workload. We provide beauty services like salon at home and
+          home care services like plumbing and carpentry, all at your doorstep.
+          Our professionals are well articulated in their profession and they
+          undergo rigorous training. Your satisfaction means the world to us.
         </Desc>
         <Desc>
-          We at Urbanscape, believe in excellence! All our certified professionals are trained to deliver expert services that will surpass your expectations.
+          We at Urbanscape, believe in excellence! All our certified
+          professionals are trained to deliver expert services that will surpass
+          your expectations.
         </Desc>
         <Desc>
-
-          We love mother nature and all the products used for our beauty services are organic and certified cruelty free.
-
+          We love mother nature and all the products used for our beauty
+          services are organic and certified cruelty free.
         </Desc>
       </Grid>
       <Grid item xs>
-
         <Title>Useful Links</Title>
         <List>
           <ListItem disablePadding>
-
-
-            <Link href="./beautyservices" color="inherit">
-              <ListItemText primary="Beauty Services" />
+            <Link href='./beautyservices' color='inherit'>
+              <ListItemText primary='Beauty Services' />
             </Link>
-
           </ListItem>
           <ListItem disablePadding>
-
-            <Link href="./carpentryservices" color="inherit">
-              <ListItemText primary="Carpentry Services" />
+            <Link href='./carpentryservices' color='inherit'>
+              <ListItemText primary='Carpentry Services' />
             </Link>
-
           </ListItem>
           <ListItem disablePadding>
-            <Link href="./plumbingservices" color="inherit">
-              <ListItemText primary="Plumbing Services" />
+            <Link href='./plumbingservices' color='inherit'>
+              <ListItemText primary='Plumbing Services' />
             </Link>
-
           </ListItem>
-          <ListItem disablePadding>
+          {/* <ListItem disablePadding>
 
             <Link href="./" color="inherit">
               <ListItemText primary="Offers" />
             </Link>
 
-          </ListItem>
+          </ListItem> */}
           <ListItem disablePadding>
-            <Link href="./" color="inherit">
-              <ListItemText primary="FAQs" />
+            <Link href='./support' color='inherit'>
+              <ListItemText primary='Support' />
             </Link>
-
           </ListItem>
           <ListItem disablePadding>
-            <Link href="./" color="inherit">
-              <ListItemText primary="Reviews" />
+            <Link href='./displayReview' color='inherit'>
+              <ListItemText primary='Reviews' />
             </Link>
           </ListItem>
         </List>
-
       </Grid>
       <Grid item xs>
         <Title>Contact</Title>
@@ -110,12 +99,12 @@ const Footer = () => {
           <Phone style={{ marginRight: '10px' }} /> +1 234 56 789
         </ContactItem>
         <ContactItem>
-          <MailOutline style={{ marginRight: '10px' }} /> contact@urbanscapeteam.dev
+          <MailOutline style={{ marginRight: '10px' }} />{' '}
+          contact@urbanscapeteam.dev
         </ContactItem>
         <Payment src='https://i.ibb.co/Qfvn4z6/payment.png' />
       </Grid>
     </Grid>
-
   )
 }
 
--- src/components/UserReviews/AddReview.js
+++ src/components/UserReviews/AddReview.js
@@ -1,58 +1,58 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Rating from '@mui/material/Rating';
-import Typography from '@mui/material/Typography';
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Rating from '@mui/material/Rating'
+import Typography from '@mui/material/Typography'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import Grid from '@mui/material/Grid'
 import CssBaseline from '@mui/material/CssBaseline'
 import Paper from '@mui/material/Paper'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
-import { TextareaAutosize } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import { TextareaAutosize } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import { BASE_URL } from '../../utils/string'
 
 export default function BasicRating() {
-  const [value, setValue] = React.useState(2);
-  const navigateHome = useNavigate();
-  console.log(value);
+  const [value, setValue] = React.useState(2)
+  const navigateHome = useNavigate()
+  console.log(value)
   const theme = createTheme()
 
-  const handleSubmit = (event)=>{
+  const handleSubmit = (event) => {
     event.preventDefault()
     const data = new FormData(event.currentTarget)
 
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    
+    var myHeaders = new Headers()
+    myHeaders.append('Content-Type', 'application/json')
+
     var raw = JSON.stringify({
-        firstname: data.get('name'),
-        review: data.get('review'),
-        ratings:value
-      })
-  
-    
+      firstname: data.get('name'),
+      review: data.get('review'),
+      ratings: value,
+    })
+
     var requestOptions = {
       method: 'POST',
       headers: myHeaders,
       body: raw,
-      redirect: 'follow'
-    };
-    
-    fetch("http://localhost:5000/api/reviews/addReview", requestOptions)
-      .then(response => response.text())
-      .then(result => {
-        if(result){
+      redirect: 'follow',
+    }
+
+    fetch(`${BASE_URL}reviews/addReview`, requestOptions)
+      .then((response) => response.text())
+      .then((result) => {
+        if (result) {
           navigateHome('/')
         }
       })
-      .catch(error => console.log('error', error));
+      .catch((error) => console.log('error', error))
   }
 
   return (
     <ThemeProvider theme={theme}>
-    <Grid container component='main' sx={{ height: '100vh' }}>
-      <CssBaseline />
-      {/* <Grid
+      <Grid container component='main' sx={{ height: '100vh' }}>
+        <CssBaseline />
+        {/* <Grid
         item
         xs={false}
         sm={4}
@@ -68,22 +68,21 @@ export default function BasicRating() {
           backgroundPosition: 'center',
         }}
       /> */}
-      {/* <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square> */}
+        {/* <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square> */}
         <Box
           sx={{
-            position: "absolute",
-        left: "50%",
-        top: "50%",
-        transform: "translate(-50%, -50%)",
-        display: "flex",
-        flexDirection:"column",
-        alignItems: "center",
-        justifyContent: "center",
+            position: 'absolute',
+            left: '50%',
+            top: '50%',
+            transform: 'translate(-50%, -50%)',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
           }}
         >
-            
           <Typography component='h1' variant='h5'>
-          Let us know how was your experience!
+            Let us know how was your experience!
           </Typography>
           <Box
             component='form'
@@ -91,7 +90,7 @@ export default function BasicRating() {
             onSubmit={handleSubmit}
             sx={{ mt: 1 }}
           >
-               <TextField
+            <TextField
               margin='normal'
               required
               fullWidth
@@ -111,34 +110,32 @@ export default function BasicRating() {
               autoComplete='review'
               type='text'
             />
-             <Box
-       sx={{
-         '& > legend': { mt: 2 },
-            position: "absolute",
-        left: "50%",
-        top: "70%",
-        transform: "translate(-50%, -50%)",
-        display: "flex",
-        flexDirection:"column",
-        alignItems: "center",
-        justifyContent: "center",
-       }}
-     >
-       {/* <Typography component="legend"></Typography> */}
-       <Rating
-         name="simple-controlled"
-       value={value}
-       size="large"
-         onChange={(event, newValue) => {
-           setValue(newValue);
-         }}
-       />
-     
-      
-     </Box>
-     <br></br>
-     <br></br>
-    
+            <Box
+              sx={{
+                '& > legend': { mt: 2 },
+                position: 'absolute',
+                left: '50%',
+                top: '70%',
+                transform: 'translate(-50%, -50%)',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              {/* <Typography component="legend"></Typography> */}
+              <Rating
+                name='simple-controlled'
+                value={value}
+                size='large'
+                onChange={(event, newValue) => {
+                  setValue(newValue)
+                }}
+              />
+            </Box>
+            <br></br>
+            <br></br>
+
             <Button
               type='submit'
               fullWidth
@@ -148,13 +145,12 @@ export default function BasicRating() {
               Submit
             </Button>
             <Grid container>
-              
               <Grid item></Grid>
             </Grid>
           </Box>
         </Box>
       </Grid>
-    {/* </Grid> */}
-  </ThemeProvider>
- );
+      {/* </Grid> */}
+    </ThemeProvider>
+  )
 }
